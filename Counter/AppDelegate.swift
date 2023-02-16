f04d08c260073ba3a8d1093e30455b6cc5197b59
vovascool <mirdydik@icloud.com>
2023-02-16 16:14:37 +0300

    Test 2

--- Counter/AppDelegate.swift
+++ Counter/AppDelegate.swift
@@ -14,7 +14,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        print("hello world")
+        print("hello world"); print("Eren YEGAA")
         return true
     }
 
