a1309f5784ef0e1fe65f51381e74b2ab7178ffb5
vovascool <mirdydik@icloud.com>
2023-02-21 21:43:34 +0300

    Создал простое приложение-счетчик. При нажатии на кнопку "Нажми меня" - значение счетчика увеличивается на 1.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var counter: Int = 0
 
+    @IBAction func buttonTapped(_ sender: Any) {
+        counter += 1
+        counterLabel.text = "Значение счетчика: \(counter)"
+    }
+    
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBOutlet weak var myButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterLabel.text = "0"
+        myButton.setTitle("Нажми меня", for: .normal)
     }
 
 
